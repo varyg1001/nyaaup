c7ecf8933af38700630dc63808a962cb366e3347
varyg <88599103+varyg1001@users.noreply.github.com>
2024-06-03 16:03:40 +0200

    Update dependencies

---
+++ install.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python3
+
+import os
+import platform
+import subprocess
+from pathlib import Path
+
+
+print("\n[*] Installing dependencies")
+subprocess.run(["poetry", "install"], check=True)
+
+if platform.system() != "Windows":
+    d = Path("~/.local/bin").expanduser()
+    d.mkdir(parents=True, exist_ok=True)
+
+    print("\n[*] Installing launcher script")
+    (d / "nyaaup").unlink(missing_ok=True)
+    (d / "nyaaup").symlink_to(Path(".venv/bin/nyaaup").resolve())
+
+    if not any(Path(x).resolve() == d.resolve() for x in os.environ["PATH"].split(os.pathsep)):
+        print(f"[!] WARNING: {d} is not in PATH.")
