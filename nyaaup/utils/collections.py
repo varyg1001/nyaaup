be2a898069726e579c9ccd6fea7a20d2396bbfb8
varyg <88599103+varyg1001@users.noreply.github.com>
2025-01-10 21:26:19 +0100

    Add as_list function

--- nyaaup/utils/collections.py
+++ nyaaup/utils/collections.py
@@ -1,3 +1,5 @@
+import itertools
+
 def first(iterable):
     return next(iter(iterable))
 
@@ -11,3 +13,21 @@ def first_or_else(iterable, default):
 
 def first_or_none(iterable):
     return first_or_else(iterable, None)
+
+def as_lists(*args):
+    """Convert any input objects to list objects."""
+    for item in args:
+        yield item if isinstance(item, list) else [item]
+
+
+def as_list(*args):
+    """
+    Convert any input objects to a single merged list object.
+
+    Example:
+    >>> as_list('foo', ['buzz', 'bizz'], 'bazz', 'bozz', ['bar'], ['bur'])
+    ['foo', 'buzz', 'bizz', 'bazz', 'bozz', 'bar', 'bur']
+    """
+    if args == (None,):
+        return []
+    return list(itertools.chain.from_iterable(as_lists(*args)))
